df77513857e09aae11dcfab4f4b8d1421bf54940
0xalexander <0xalexander@proton.me>
2023-11-12 00:05:26 +0200

    improve: scroll margin

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 
 export default function Contact() {
   return (
-    <section id="contact" className="scroll-mt-16 lg:scroll-mt-24 lg:mt-16">
+    <section id="contact" className="scroll-mt-16 lg:mt-16">
       <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-background/0 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">
           Get In Touch
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -46,7 +46,7 @@ const jobProjects = [
 
 export default function Projects() {
   return (
-    <section id="projects" className="scroll-mt-16 lg:scroll-mt-24 lg:mt-16">
+    <section id="projects" className="scroll-mt-16 lg:mt-16">
       <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-background/0 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">
           Projects
